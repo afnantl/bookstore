7f0b62ef4a42e89b2ba24c282c4abf2601fffd8a
afnantl <91306698+afnantl@users.noreply.github.com>
2021-09-29 16:01:42 +0530

    Update books.js

--- services/books.js
+++ services/books.js
@@ -1,5 +1,18 @@
-const books=[]
+const books=[];
+var i = 0;
 
-  module.exports=books;
+function addBook(req){
+  let newBook = {
+    "id":++i,
+    "name":req.body.name,
+    "author":req.body.author,
+    "edition":req.body.edition
+    }
+    books.push(newBook);
+    return newBook;
+  
+}
+
+  module.exports = { addBook };
 
 
