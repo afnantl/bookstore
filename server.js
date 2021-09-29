af3eb381465c1f985a92536996809e1d6665dc6f
afnantl <91306698+afnantl@users.noreply.github.com>
2021-09-29 14:33:21 +0530

    Update and rename store.js to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express()
+app.use(express.json());
+
+var routes = require("./routes/allroutes");
+app.use("/", routes);
+
+app.listen(8000,()=>{
+    console.log('listening to port 8000');
+})
