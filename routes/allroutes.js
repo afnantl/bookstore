100384d1602718ede83c2d0b1d7a052e79b6f004
afnantl <91306698+afnantl@users.noreply.github.com>
2021-10-01 17:48:42 +0530

    Update allroutes.js

--- routes/allroutes.js
+++ routes/allroutes.js
@@ -2,27 +2,60 @@ var express = require("express");
 var router = express.Router();
 const book = require("../services/books");
 
-router.get('/allbooks',async function(req,res){
-    let allbooks = book.getBook();
-    res.json(allbooks);
+router.get('/allbooks', function(req,res){
+    let stat;
+    let allbooks = book.allBooks();
+    if(allbooks.msg == "success"){
+        stat=200;
+     } else {
+        stat=404;
+     }
+     res.status(stat).json(allbooks);
+});
+router.get('*', function (req, res) {
+    res.status(404).json({msg:"Invalid route"});
 });
 
-router.post('/add',async (req, res) => {
+router.post('/add', function (req,res) {
+    let stat;
     let addedBook = book.addBook(req);
-    res.json(addedBook);
+    if(addedBook.msg == "success"){
+        stat=200;
+     } else {
+        stat=404;
+     }
+     res.status(stat).json(addedBook);
 })
+router.post('*', function (req, res) {
+    res.status(404).json({msg:"Invalid route"});
+});
 
-router.put('/update/:id',function(req,res){
-
+router.put('/update/:id', function (req,res){
+    let stat;
     let updateBooks=book.updateBook(req);
-    res.json(updateBooks);
-})
+    if(updateBooks.msg == "success"){
+       stat=200;
+    } else {
+       stat=404;
+    }
+    res.status(stat).json(updateBooks);
+});
+router.put('*', function (req, res) {
+    res.status(404).json({msg:"Invalid route"});
+});
 
 
 router.delete('/delete/:id',function(req,res){
     let deleteBooks = book.deleteBook(req);
-    res.json(deleteBooks);
-})
+    if(deleteBooks.msg == "success"){
+        res.status(200).json(deleteBooks);
+    } else {
+    res.status(404).json(deleteBooks);
+    }
+});
+router.delete('*', function (req, res) {
+    res.status(404).json({msg:"Invalid route"});
+});
 
 
 module.exports = router;
